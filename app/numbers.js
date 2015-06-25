e8e424301e341e7a86003a5673627e07c20fa75a
Juan Manuel Rodriguez <rodriguezjuanmanuelar@gmail.com>
2015-06-25 16:41:59 -0300

    added logicOperators, modules, numbers and objects

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,31 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    var selectBit = '',
+        result = 0;
+    result = num.toString(2);
+    selectBit = Number(result.charAt(result.length - bit));
+    return selectBit;
   },
 
   base10: function(str) {
-
+    var numConvert = parseInt(Number(str),2).toString(10);
+    return Number(numConvert);
   },
 
   convertToBinary: function(num) {
-
+    var strNum = num.toString(2),
+        digits = 8;
+    function pad (str,max) {
+      str.toString();
+      return str.length < max ? pad("0" + str, max) : str;
+    };
+    return pad(strNum,digits);
   },
 
   multiply: function(a, b) {
-
+    var precisionNumber = (a*b).toPrecision(b.toString().length-2);
+    console.log(b.toString().length);
+    return Number(precisionNumber);
   }
 };
--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
+    var arr=[];
+    for (var name in obj){
+      if (obj.hasOwnProperty(name)){
+        arr.push(name +': '+obj[name]);
+      };
+    };
+    console.log(arr);
+    return arr;
   }
 };
