f764e133014b5442a270c0b20507791d840425d0
Juan Manuel Rodriguez <rodriguezjuanmanuelar@gmail.com>
2015-06-26 17:00:56 -0300

    recursion

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    console.log(data.files);
+    console.log(data.files[1].files);
+    var arr=[];
 
+    for (var i=0; i < data.files.length; i++){
+      if ((typeof data.files[i]) === "string"){
+        console.log(data.files[i]);
+        return data.files[i];
+      }
+      else {
+        //TODO: recursion
+        listFiles (data.files,dirName);
+      };
+    };
   },
 
   permute: function(arr) {
