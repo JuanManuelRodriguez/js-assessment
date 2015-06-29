ce1707b82e2685d4ab2504479d916ef1758fd3f8
Juan Manuel Rodriguez <rodriguezjuanmanuelar@gmail.com>
2015-06-29 17:57:23 -0300

    recursion - permutations and fibonacci

--- app/recursion.js
+++ app/recursion.js
@@ -33,11 +33,43 @@ exports.recursionAnswers = {
   },
 
   permute: function(arr) {
-    
+    var arrResult = [], arrTemp = [];
+
+    function permuteAnswer(a){
+      var i,len,item;
+      for (i=0,len= a.length; i < len;i++){
+        item = a.splice(i,1)[0];
+        arrTemp.push(item);
+        if (a.length){
+          permuteAnswer(a);
+        }
+        else{
+          showResult();
+        };
+        arr.splice(i,0,item);
+        arrTemp.pop();
+      };
+      return arrResult;
+    };
+    function showResult(){
+      arrResult.push(arrTemp.slice());
+    };
+    return (permuteAnswer(arr));
   },
 
   fibonacci: function(n) {
-
+    var first = 0, second = 1, sum = 0;
+    function fib(num){
+      if (num > 1){
+        sum = first + second;
+        first = second;
+        second= sum;
+        fib(num-1);
+      }
+      else{return sum};
+      return sum;
+    };
+    return (fib(n));
   },
 
   validParentheses: function(n) {
